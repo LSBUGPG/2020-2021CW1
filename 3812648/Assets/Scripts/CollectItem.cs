ddbf9a4bfcad7116c562dd79ad8da0fd6309e4e6
Paul Sinnett <paul.sinnett@gmail.com>
2021-01-30 20:45:26 +0000

    3812648

---
+++ 3812648/Assets/Scripts/CollectItem.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CollectItem : MonoBehaviour
+{
+
+    public TextMeshProUGUI itemText;
+    public static int items;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        items = 0;
+
+        SetItemText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int GetItemCount()
+    {
+        return items;
+    }
+
+    void SetItemText()
+    {
+        itemText.text = "Itmes: " + items.ToString();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            items = items + 1;
+
+            SetItemText();
+        }
+    }
+}
---
+++ 3812648/Assets/Scripts/VictoryReveal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryReveal : MonoBehaviour
+{
+    private static int itemCount;
+    public GameObject victoryCapsule;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        victoryCapsule.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        itemCount = CollectItem.items;
+
+        if(itemCount > 3)
+        {
+            victoryCapsule.SetActive(true);
+        }
+    }
+}
