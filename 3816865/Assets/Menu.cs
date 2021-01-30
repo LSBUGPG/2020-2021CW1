4c3b404fa8aa336b7e942441b9fdeafc465cc7e9
Paul Sinnett <paul.sinnett@gmail.com>
2021-01-30 20:07:52 +0000

    3816865

---
+++ 3816865/Assets/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    public string SceneName;
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void QuitGame ()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+
+}
