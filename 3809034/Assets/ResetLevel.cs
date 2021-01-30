de7a77aaeb781fcf86469754f0a61685aa767b22
Paul Sinnett <paul.sinnett@gmail.com>
2021-01-30 20:43:15 +0000

    3809034

---
+++ 3809034/Assets/ResetLevel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ResetLevel : MonoBehaviour
+{
+    private Scene scene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scene = SceneManager.GetActiveScene();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTrigggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            Application.LoadLevel(scene.name);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        }
+        
+
+
+    }
+}
