79e4a8bc3223e806dccf16d1279113362fa25e3b
Paul Sinnett <paul.sinnett@gmail.com>
2021-01-30 20:17:41 +0000

    3817486

---
+++ 3817486/Assets/Scripts/ChasePlayer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChasePlayer : MonoBehaviour
+{
+    private Transform player;
+    private float dist;
+    public float moveSpeed;
+    public float howclose;
+
+   
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        dist = Vector3.Distance(player.position, transform.position);
+
+        if(dist <= howclose)
+        {
+            transform.LookAt(player);
+            GetComponent<Rigidbody>().AddForce(transform.forward * moveSpeed);
+        }
+
+  }
+
+   
+
+
+
+
+
+
+
+
+
+}
---
+++ 3817486/Assets/Scripts/PlayerRespawn.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRespawn : MonoBehaviour
+{
+
+    [SerializeField] private Transform Player;
+    [SerializeField] private Transform RespawnPoint;
+
+    private void OnTriggerEnter(Collider player)
+    {
+        Player.transform.position = RespawnPoint.transform.position;
+    }
+}
