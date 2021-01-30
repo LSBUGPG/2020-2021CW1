0780713c0b86c7f9a09120659f9024acc2b64df5
Paul Sinnett <paul.sinnett@gmail.com>
2021-01-30 19:29:59 +0000

    3818095

---
+++ 3818095/Assets/scripts/Quests/Quest001Take.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Quest001Take : MonoBehaviour
+{
+
+	public float TheDistance;
+	public GameObject ActionDisplay;
+	public GameObject ActionText;
+	public GameObject UIQuest;
+	public GameObject ThePlayer;
+	public GameObject NoticeCam;
+
+	void Update()
+	{
+		TheDistance = PlayerCasting.DistanceFromTarget;
+	}
+
+	void OnMouseOver()
+	{
+		if (TheDistance <= 16)
+		{
+			ActionDisplay.SetActive(true);
+			ActionText.SetActive(true);
+		}
+
+		if (Input.GetButtonDown("Action"))
+		{
+			if (TheDistance <= 16)
+			{
+				Screen.lockCursor = false;
+				Cursor.visible = true;
+			
+				ActionDisplay.SetActive(false);
+				ActionText.SetActive(false);
+				UIQuest.SetActive(true);
+				NoticeCam.SetActive(true);
+				ThePlayer.SetActive(false);
+			}
+		}
+	}
+
+	void OnMouseExit()
+	{
+		ActionDisplay.SetActive(false);
+		ActionText.SetActive(false);
+	}
+
+
+}
+
