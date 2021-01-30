854bdbc52a3ae004edff500fae1419b1385845a3
Paul Sinnett <paul.sinnett@gmail.com>
2021-01-30 20:22:37 +0000

    3706006

---
+++ 3706006/Assets/Scripts/Corner2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Corner2 : MonoBehaviour
+{
+    ShapeTrace2 game;
+    public Color normal;
+    public Color highlight;
+    MeshRenderer mr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        game = GameObject.FindGameObjectWithTag("ShapeTrace2").GetComponent<ShapeTrace2>();
+        mr = GetComponent<MeshRenderer>();
+        mr.material.color = normal;
+    }
+
+    private void OnMouseDown()
+    {
+        mr.material.color = highlight;
+        game.AddCorner(this.gameObject);
+   
+    }
+}
---
+++ 3706006/Assets/Scripts/GrabAndDrag.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabAndDrag : MonoBehaviour
+{
+    public GameObject item;
+    public GameObject tempParent;
+    public Transform guide;
+
+    Vector3 originalPos;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        item.GetComponent<Rigidbody>().useGravity = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            OnButtonDown();
+        }
+        else if (Input.GetKeyUp(KeyCode.H))
+        {
+            OnButtonUp();
+        }
+
+    }
+
+    void OnButtonDown()
+    {
+        item.GetComponent<Rigidbody>().useGravity = false;
+        item.GetComponent<Rigidbody>().isKinematic = true;
+        originalPos = item.transform.position;
+        item.transform.position = guide.transform.position;
+        item.transform.rotation = guide.transform.rotation;
+        item.transform.parent = tempParent.transform;
+
+    }
+
+    void OnButtonUp()
+    {
+        item.GetComponent<Rigidbody>().useGravity = true;
+        item.GetComponent<Rigidbody>().isKinematic = false;
+        item.transform.parent = null;
+        item.transform.position = originalPos;
+
+    }
+}
