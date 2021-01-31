b46ab9cf2d660fdd26deb224ce9ccd06b2605658
Paul Sinnett <paul.sinnett@gmail.com>
2021-01-31 00:41:50 +0000

    3930556

---
+++ 3930556/Assets/Scripts/OnTriggerLoadEndLevel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnTriggerLoadEndLevel : MonoBehaviour
+{
+    public GameObject enterText;
+    public string levelToLoad;
+    void Start()
+    {
+        enterText.SetActive(false);
+    }
+
+    private void OnTriggerStay(Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            enterText.SetActive(true);
+        if (Input.GetButtonDown("use"))
+            {
+                SceneManager.LoadScene(levelToLoad);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider Player)
+    {
+        if (Player.gameObject.tag == "Player")
+        {
+            enterText.SetActive(false);
+        }
+    }
+}
