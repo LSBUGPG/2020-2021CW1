e79b4fc0e0ee51d84758eaee35aa04e6ce38dc7c
Paul Sinnett <paul.sinnett@gmail.com>
2021-01-31 00:55:37 +0000

    3725916

---
+++ 3725916/Assets/AiFollow.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AiFollow : MonoBehaviour
+{
+    private NavMeshAgent Enemy;
+
+    public Transform Player;
+
+    public float EnemyDistanceRun = 4.0f;
+
+    public Transform SpawnPoint;
+
+    public GameObject Prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Enemy = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, Player.transform.position);
+
+        //Run Towards Player
+
+        if (distance < EnemyDistanceRun)
+        {
+            Vector3 dirToPlayer = transform.position - Player.transform.position;
+
+            Vector3 newPos = transform.position - dirToPlayer;
+
+            Enemy.SetDestination(newPos);
+        }
+    }
+
+    void OnTriggerEnter ()
+    {
+        Instantiate(Prefab, SpawnPoint.position, SpawnPoint.rotation);
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            print("Item picked up");
+            Destroy(gameObject);
+        }
+    }
+
+
+
+}
+
---
+++ 3725916/Assets/Movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 6f;
+
+    //So the player turns more smoothly in another direction
+    public float turnsmoothTime = 0.05f;
+    float turnsmoothVelocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+        
+            controller.Move(direction * speed * Time.deltaTime);
+        }
+    }
+}
